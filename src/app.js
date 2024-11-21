598fc827bd3539bb5a59ed7cfdd040c241612f40
ALBANTA LD <albanta.leondelgado@gmail.com>
2024-11-21 03:48:54 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,57 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const symbolList = ["♠", "♥", "◈", "♣"];
+  const valueList = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const getRandomizer = list => {
+    let rnd = Math.random();
+    let item = Math.floor(rnd * list.length);
+    return list[item];
+  };
+
+  const getRandomSymbol = () => {
+    return getRandomizer(symbolList);
+  };
+
+  const getRandomValue = () => {
+    return getRandomizer(valueList);
+  };
+
+  const seterizer = (selector, value) => {
+    document.querySelector(selector).innerHTML = value;
+  };
+
+  const setSymbolValue = (symbol, selector) => {
+    seterizer(selector, symbol);
+  };
+
+  const setValueValue = value => {
+    seterizer("#value", value);
+  };
+
+  const playCards = () => {
+    const symbol = getRandomSymbol();
+    const value = getRandomValue();
+    setSymbolValue(symbol, "#symbol1");
+    setSymbolValue(symbol, "#symbol2");
+    setValueValue(value);
+  };
+
+  document.getElementById("generate").addEventListener("click", playCards);
+
+  playCards();
 };
